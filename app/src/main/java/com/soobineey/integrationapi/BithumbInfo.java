ad21f6ff6b3b6447b3b3700eeef3e3311dcfe7be
soobineey <tnehfdl2011@gmail.com>
2020-06-09 18:06:27 +0900

    code clear

--- app/src/main/java/com/soobineey/integrationapi/BithumbInfo.java
+++ app/src/main/java/com/soobineey/integrationapi/BithumbInfo.java
@@ -1,6 +1,5 @@
 package com.soobineey.integrationapi;
 
-import android.util.Log;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
--- app/src/main/java/com/soobineey/integrationapi/CoinoneInfo.java
+++ app/src/main/java/com/soobineey/integrationapi/CoinoneInfo.java
@@ -7,7 +7,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Locale;
 
 public class CoinoneInfo {
 
